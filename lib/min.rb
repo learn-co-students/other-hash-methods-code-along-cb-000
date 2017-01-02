96f1ad48bfa45f55462cb27df75588289b5b5afb
Drew Vollrath <andre@vollrath.ca>
2017-01-02 22:56:14 +0000

    Done.

--- lib/min.rb
+++ lib/min.rb
@@ -1,13 +1,20 @@
 require 'pry'
 
-#This is the groceries hash we'll be passing in to the get_the_min method
-# groceries = {
-#  dairy: ["milk", "yogurt", "cheese"],
-#  vegetable: ["carrots", "broccoli", "cucumbers"],
-#  meat: ["chicken", "steak", "salmon"],
-#  grains: ["rice", "pasta"]
-# }
+
 
 def get_the_min(groceries)
-  #code your solution here!
+  min_item = []
+  groceries.each do |key, item|
+    min_item << item.min
+  end
+  min_item.min
 end
+
+groceries = {
+  dairy: ["milk", "yogurt", "cheese"],
+  vegetable: ["carrots", "broccoli", "cucumbers"],
+  meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+}
+
+get_the_min(groceries)
