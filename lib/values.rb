834ea83b7c7d431ad9cb7f4bfbc9679e6f1af86f
Dan Blondell <danblondell@gmail.com>
2017-07-21 02:56:01 +0000

    Done.

--- lib/values.rb
+++ lib/values.rb
@@ -10,4 +10,15 @@ require 'pry'
 
 def get_the_values(groceries)
   #code your solution here!
+
+  # THE LONG WAY
+  # values_array = groceries.values
+  # groceries_array = []
+  # values_array.each do | sub_array |
+  #   sub_array.each { | item | groceries_array << item }
+  # end
+  # groceries_array
+
+  # THE EASY WAY
+  groceries.values.flatten
 end
